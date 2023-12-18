ceb2eeddb2324080473bf505699f9943f9213cda
tamlt2704 <tamlt2704@gmail.com>
2023-12-18 22:20:20 +0800

    problems-ConcatenationOfArray

---
+++ src/main/java/com/leetcode/problems/ConcatenationOfArray.java
@@ -0,0 +1,17 @@
+package com.leetcode.problems;
+
+import java.util.Collections;
+
+public class ConcatenationOfArray {
+    class Solution {
+        public int[] getConcatenation(int[] nums) {
+            int[] ans = new int[2 * nums.length];
+            int n = nums.length;
+            for (int i = 0; i < n; i++) {
+                ans[i] = nums[i];
+                ans[i + n] = nums[i];
+            }
+            return ans;
+        }
+    }
+}
